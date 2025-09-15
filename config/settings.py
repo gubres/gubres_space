72d815e7e336a5020f8c6d37eb49d8dc7d0f76cd
gubres <gustavoiago@hotmail.com>
2025-09-15 20:21:35 +0200

    Mejora en la logica ORM, añandindo la base de datos del objeto/model. Generada y configurada la pagina de admin, filtrado de las fotos en la view. Aplicacion de la logica modular en los templates

--- config/settings.py
+++ config/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'galeria'
+    'galeria.apps.GaleriaConfig'
 ]
 
 MIDDLEWARE = [
@@ -121,6 +121,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'config/static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
